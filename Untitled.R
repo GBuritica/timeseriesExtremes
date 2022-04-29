2b0194e9530c9b767d065a554a4c9610fb2043a0
Gloria Buriticá <gloriaburit@gmail.com>
2022-04-29 15:03:44 +0200

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -5,20 +5,25 @@
 ##### Gloria Buriticá
 ####  Package name: timeseriesExtremes
 ####  Package: Class + Generic + Method.
-####  
-####  Each class must have: 
+####
+####  Each class must have:
 ###    constructor (must), validator (ok if simple), helper (for external users)
 
 ### References: https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
-###             https://adv-r.hadley.nz/environments.html?q=package#search-path 
+###             https://adv-r.hadley.nz/environments.html?q=package#search-path
+###             https://github.com/mvuorre/exampleRPackage
 #######################################################################
 library(sloop)
-library('devtools')
-library('roxygen2')
+library('devtools')             ## for developing
+library('roxygen2')             ## for documenting
+library(usethis)                ## for developing
 ##
-## 
+##
 
-create("timeseriesExtremes")
+
+use_readme_rmd()  ## Imports usethis, creates an Rmarkdown to document package.
+use_data_raw()    ## Adding data
+## create("timeseriesExtremes") # Creates new  package using Devtools
 
 f <- factor(c("a", "b", "c"))
 
@@ -29,6 +34,6 @@ x <- structure(list(), class = "timeseriesExtremes_time_series")
 new_timeseries <- function(x = double()){ ## constructor
   stopifnot(is.double(x))
   structure(x, class = "timeseriesExtremes_time_series")
-} 
+}
 
 
